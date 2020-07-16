cbd945e88c2c90773263b10befcd6f6eb6fdc3bc
Khamthalar5566 <tikkhamthalar@gmail.com>
2020-07-16 08:32:10 +0700

    update 16-07-2020

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'requetFix'`, () => {
+  it(`should have as title 'LAOSKYWAY'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('requetFix');
+    expect(app.title).toEqual('LAOSKYWAY');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('requetFix app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('LAOSKYWAY app is running!');
   });
 });
